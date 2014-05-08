fdf563561539a31ef3e1a3d4e5ef6987e8f91554
markkleeb <kleeb930@gmail.com>
2014-05-07 20:22:18 -0400

    initial commit

---
+++ application.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390});
+	$(".vimeo").colorbox({iframe:true, innerWidth:500, innerHeight:409});
+	$('nav a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top - 90
+	    }, 500);
+	    return false;
+	});
+	$.colorbox.settings.opacity = 0.8;
+	$('.field-1').click(function(){
+		$.colorbox({href:"field1.html"});
+	});
+	$('.crystal-brawl').click(function(){
+		$.colorbox({href:"crystalbrawl.html"});
+	});
+
+	$('.mini-thumb-link').colorbox({rel:'group5', transition:'none'})
+});
